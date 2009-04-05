1739e701df2f532a115d3d64a958f25a49943323
Ali Abdallah <ali.slackware@gmail.com>
2009-04-05 18:31:15 +0000

    Include missing config.h in xfpm-battery-info
    (Old svn revision: 7116)

--- src/xfpm-battery-info.c
+++ src/xfpm-battery-info.c
@@ -18,6 +18,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <libxfce4util/libxfce4util.h>
 #include <libxfcegui4/libxfcegui4.h>
 
