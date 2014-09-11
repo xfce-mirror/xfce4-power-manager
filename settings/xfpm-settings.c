0ea4b967127dfea6b2a7ca433e07b8b317bf71b9
Eric Koegel <eric.koegel@gmail.com>
2014-09-11 20:50:26 +0300

    Conditionally use xfce_dialog_show_help_with_version
    Only build in the new xfce_dialog_show_help_with_version function
    if the system has libxfce4ui 4.11.1, otherwise use the old help
    function in order to still support systems with Xfce 4.10.

--- settings/xfpm-settings.c
+++ settings/xfpm-settings.c
@@ -1997,7 +1997,11 @@ static void dialog_response_cb (GtkDialog *dialog, gint response, XfconfChannel 
     switch(response)
     {
 	case GTK_RESPONSE_HELP:
-	    xfce_dialog_show_help_with_version (GTK_WINDOW (dialog), "xfce4-power-manager", "start", NULL, XFPM_VERSION_SHORT);
+#if LIBXFCE4UI_CHECK_VERSION(4, 11, 1)
+	    xfce_dialog_show_help_with_version (NULL, "xfce4-power-manager", "start", NULL, XFPM_VERSION_SHORT);
+#else
+	    xfce_dialog_show_help (NULL, "xfce4-power-manager", "start", NULL);
+#endif
 	    break;
 	default:
 	    settings_quit (GTK_WIDGET (dialog), channel);
