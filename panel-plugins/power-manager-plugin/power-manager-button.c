53fdf5ccc0d3195c410adf894d9f89f48e48ec5c
Eric Koegel <eric.koegel@gmail.com>
2015-05-27 18:43:53 +0300

    Fix panel plugin icon loading
    This reverts 0dd1196b8fc5c8634f858c4f68e5ff7360a4da01
    and adds an additional check for UPower returning "" for the icon
    name.

--- panel-plugins/power-manager-plugin/power-manager-button.c
+++ panel-plugins/power-manager-plugin/power-manager-button.c
@@ -407,7 +407,13 @@ power_manager_button_update_device_icon_and_details (PowerManagerButton *button,
         DBG("this is the display device, updating");
         /* it is! update the panel button */
         g_free (button->priv->panel_icon_name);
-
+#ifdef XFCE_PLUGIN
+        g_object_get (device,
+                      "icon-name", &icon_name,
+                      NULL);
+        if (icon_name == NULL || g_strcmp0(icon_name, "") == 0)
+            icon_name = g_strdup (PANEL_DEFAULT_ICON);
+#endif
         button->priv->panel_icon_name = g_strdup (icon_name);
         power_manager_button_set_icon (button);
         /* update tooltip */
