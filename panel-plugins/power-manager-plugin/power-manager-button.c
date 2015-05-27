1e5d59f233f7cbd493e94a8aaa2c2b45e599370c
Eric Koegel <eric.koegel@gmail.com>
2015-05-27 19:04:45 +0300

    Same check for systray

--- panel-plugins/power-manager-plugin/power-manager-button.c
+++ panel-plugins/power-manager-plugin/power-manager-button.c
@@ -382,7 +382,7 @@ power_manager_button_update_device_icon_and_details (PowerManagerButton *button,
     details = get_device_description (button->priv->upower, device);
 
     /* If UPower doesn't give us an icon, just use the default */
-    if (icon_name == NULL)
+    if (icon_name == NULL || g_strcmp0(icon_name, "") == 0)
         icon_name = g_strdup (PANEL_DEFAULT_ICON);
 
     pix = gtk_icon_theme_load_icon (gtk_icon_theme_get_default (),
