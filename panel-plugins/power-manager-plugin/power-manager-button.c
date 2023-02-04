62b741b21d704417d0ee1172ccae50237b096869
Gaël Bonithon <gael@xfce.org>
2023-02-04 21:23:18 +0100

    panel-plugin: Add missing ref on image
    Fixes various critical especially when switching from plugged to
    battery.

--- panel-plugins/power-manager-plugin/power-manager-button.c
+++ panel-plugins/power-manager-plugin/power-manager-button.c
@@ -552,6 +552,7 @@ power_manager_button_update_device_icon_and_details (PowerManagerButton *button,
     /* update the image, keep track of the signal ids and the img
      * so we can disconnect it later */
     battery_device->img = gtk_image_new_from_surface (battery_device->surface);
+    g_object_ref (battery_device->img);
 
 G_GNUC_BEGIN_IGNORE_DEPRECATIONS
     gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(battery_device->menu_item), battery_device->img);
