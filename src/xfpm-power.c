xfce-mirror/xfce4-power-manager

commit 7b6cc24e8b7d587675efeb2d567165b15a03c414

Do not check repeatedly for logind running
Neither efficient nor secure: if for some reason the test fails when
creating the XfceSystemd instance but then succeeds, it will probably
lead to a crash.

(cherry picked from commit 92b757a56febd6d26794f9710e0cc4f486e388c1)

src/xfpm-power.c
@@ -204,7 +204,7 @@ static void
 xfpm_power_check_polkit_auth (XfpmPower *power)
 {
   const char *suspend = NULL, *hibernate = NULL;
-  if (LOGIND_RUNNING())
+  if (power->priv->systemd != NULL)
   {
     XFPM_DEBUG ("using logind suspend backend");
     suspend   = POLKIT_AUTH_SUSPEND_LOGIND;
@@ -297,7 +297,7 @@ xfpm_power_get_properties (XfpmPower *power)
   gboolean lid_is_closed;
   gboolean lid_is_present;
 
-  if ( LOGIND_RUNNING () )
+  if (power->priv->systemd != NULL)
   {
     g_object_get (G_OBJECT (power->priv->systemd),
                   "can-suspend", &power->priv->can_suspend,
@@ -448,7 +448,7 @@ xfpm_power_sleep (XfpmPower *power, const gchar *sleep_time, gboolean force)
      * - if ConsoleKit2 is running then use it
      * - if everything else fails use our built-in fallback
      */
-  if ( LOGIND_RUNNING () )
+  if (manager->priv->systemd != NULL)
   {
     xfpm_systemd_sleep (power->priv->systemd, sleep_time, &error);
   }
@@ -575,7 +575,7 @@ xfpm_power_add_actions_to_notification (XfpmPower *power, NotifyNotification *n)
 {
   gboolean can_shutdown;
 
-  if ( LOGIND_RUNNING () )
+  if (power->priv->systemd != NULL)
   {
     g_object_get (G_OBJECT (power->priv->systemd),
                   "can-shutdown", &can_shutdown,
@@ -656,7 +656,7 @@ xfpm_power_show_critical_action_gtk (XfpmPower *power)
   const gchar *message;
   gboolean can_shutdown;
 
-  if ( LOGIND_RUNNING () )
+  if (power->priv->systemd != NULL)
   {
     g_object_get (G_OBJECT (power->priv->systemd),
                   "can-shutdown", &can_shutdown,
@@ -1627,7 +1627,7 @@ static gboolean xfpm_power_dbus_shutdown (XfpmPower *power,
   GError *error = NULL;
   gboolean can_reboot;
 
-  if ( LOGIND_RUNNING () )
+  if (power->priv->systemd != NULL)
   {
     g_object_get (G_OBJECT (power->priv->systemd),
                   "can-shutdown", &can_reboot,
@@ -1649,7 +1649,7 @@ static gboolean xfpm_power_dbus_shutdown (XfpmPower *power,
     return TRUE;
   }
 
-  if ( LOGIND_RUNNING () )
+  if (manager->priv->systemd != NULL)
     xfpm_systemd_shutdown (power->priv->systemd, &error);
   else
     xfpm_console_kit_shutdown (power->priv->console, &error);
@@ -1675,7 +1675,7 @@ xfpm_power_dbus_reboot   (XfpmPower *power,
   GError *error = NULL;
   gboolean can_reboot;
 
-  if ( LOGIND_RUNNING () )
+  if (power->priv->systemd != NULL)
   {
     g_object_get (G_OBJECT (power->priv->systemd),
                   "can-restart", &can_reboot,
@@ -1697,7 +1697,7 @@ xfpm_power_dbus_reboot   (XfpmPower *power,
     return TRUE;
   }
 
-  if ( LOGIND_RUNNING () )
+  if (manager->priv->systemd != NULL)
     xfpm_systemd_reboot (power->priv->systemd, &error);
   else
     xfpm_console_kit_reboot (power->priv->console, &error);
@@ -1782,7 +1782,7 @@ xfpm_power_dbus_can_reboot (XfpmPower * power,
 {
   gboolean can_reboot;
 
-  if ( LOGIND_RUNNING () )
+  if (power->priv->systemd != NULL)
   {
     g_object_get (G_OBJECT (power->priv->systemd),
                   "can-reboot", &can_reboot,
@@ -1809,7 +1809,7 @@ xfpm_power_dbus_can_shutdown (XfpmPower * power,
 {
   gboolean can_shutdown;
 
-  if ( LOGIND_RUNNING () )
+  if (power->priv->systemd != NULL)
   {
     g_object_get (G_OBJECT (power->priv->systemd),
                   "can-shutdown", &can_shutdown,

src/xfpm-manager.c
@@ -303,7 +303,7 @@ xfpm_manager_shutdown (XfpmManager *manager)
 {
   GError *error = NULL;
 
-  if ( LOGIND_RUNNING () )
+  if (manager->priv->systemd != NULL)
     xfpm_systemd_shutdown (manager->priv->systemd, &error );
   else
     xfpm_console_kit_shutdown (manager->priv->console, &error );
@@ -418,7 +418,7 @@ xfpm_manager_lid_changed_cb (XfpmPower *power, gboolean lid_is_closed, XfpmManag
   XfpmLidTriggerAction action;
   gboolean on_battery, logind_handle_lid_switch;
 
-  if ( LOGIND_RUNNING() )
+  if (manager->priv->systemd != NULL)
   {
     g_object_get (G_OBJECT (manager->priv->conf),
                   LOGIND_HANDLE_LID_SWITCH, &logind_handle_lid_switch,
@@ -641,10 +641,10 @@ xfpm_manager_inhibit_sleep_systemd (XfpmManager *manager)
   const char *why = "xfce4-power-manager handles these events";
   const char *mode = "block";
 
-  if (g_strcmp0(what, "") == 0)
+  if (manager->priv->systemd == NULL)
     return -1;
 
-  if (!(LOGIND_RUNNING()))
+  if (g_strcmp0 (what, "") == 0)
     return -1;
 
   XFPM_DEBUG ("Inhibiting systemd sleep: %s", what);
@@ -964,7 +964,7 @@ GHashTable *xfpm_manager_get_config (XfpmManager *manager)
 
   hash = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, g_free);
 
-  if ( LOGIND_RUNNING () )
+  if (manager->priv->systemd != NULL)
   {
     g_object_get (G_OBJECT (manager->priv->systemd),
                   "can-shutdown", &can_shutdown,

commit 1522f4af9e2f9c395d67a11bf2bf02d5ed2fab29

Fix typos in previous backported commit
Fixes: 7b6cc24e8b7d587675efeb2d567165b15a03c414

src/xfpm-power.c
@@ -448,7 +448,7 @@ xfpm_power_sleep (XfpmPower *power, const gchar *sleep_time, gboolean force)
      * - if ConsoleKit2 is running then use it
      * - if everything else fails use our built-in fallback
      */
-  if (manager->priv->systemd != NULL)
+  if (power->priv->systemd != NULL)
   {
     xfpm_systemd_sleep (power->priv->systemd, sleep_time, &error);
   }
@@ -1649,7 +1649,7 @@ static gboolean xfpm_power_dbus_shutdown (XfpmPower *power,
     return TRUE;
   }
 
-  if (manager->priv->systemd != NULL)
+  if (power->priv->systemd != NULL)
     xfpm_systemd_shutdown (power->priv->systemd, &error);
   else
     xfpm_console_kit_shutdown (power->priv->console, &error);
@@ -1697,7 +1697,7 @@ xfpm_power_dbus_reboot   (XfpmPower *power,
     return TRUE;
   }
 
-  if (manager->priv->systemd != NULL)
+  if (power->priv->systemd != NULL)
     xfpm_systemd_reboot (power->priv->systemd, &error);
   else
     xfpm_console_kit_reboot (power->priv->console, &error);