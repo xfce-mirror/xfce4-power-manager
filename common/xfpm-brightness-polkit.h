c53bdb0f8c8a4e1f73a6fbb8670d279e73189d42
Gaël Bonithon <gael@xfce.org>
2023-11-07 19:40:07 +0100

    Make XfpmBrightness abstract and add X11 and polkit impls

---
+++ common/xfpm-brightness-polkit.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2023 Gaël Bonithon <gael@xfce.org>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __XFPM_BRIGHTNESS_POLKIT_H__
+#define __XFPM_BRIGHTNESS_POLKIT_H__
+
+#include "xfpm-brightness.h"
+
+G_BEGIN_DECLS
+
+#define XFPM_TYPE_BRIGHTNESS_POLKIT (xfpm_brightness_polkit_get_type ())
+G_DECLARE_FINAL_TYPE (XfpmBrightnessPolkit, xfpm_brightness_polkit, XFPM, BRIGHTNESS_POLKIT, XfpmBrightness)
+
+G_END_DECLS
+
+#endif /* __XFPM_BRIGHTNESS_POLKIT_H__ */
