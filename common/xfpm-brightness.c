xfce-mirror/xfce4-power-manager

commit 32519efa77e348af05b0abb3454c4fedd9e43077

Fix g_spawn*_sync() wait status management
Completes: 62d7fd9755b12d3289e887d3fbc065f8edf6b022

common/xfpm-brightness.c
@@ -295,29 +295,21 @@ xfpm_brightness_setup_xrandr (XfpmBrightness *brightness)
 static gint
 xfpm_brightness_helper_get_value (const gchar *argument)
 {
-  gboolean ret;
   GError *error = NULL;
   gchar *stdout_data = NULL;
-  gint exit_status = 0;
+  gint status;
   gint value = -1;
-  gchar *command = NULL;
+  gchar *command = g_strdup_printf (SBINDIR "/xfpm-power-backlight-helper --%s", argument);
 
-  command = g_strdup_printf (SBINDIR "/xfpm-power-backlight-helper --%s", argument);
-  ret = g_spawn_command_line_sync (command,
-                                   &stdout_data, NULL, &exit_status, &error);
-  if ( !ret )
+  XFPM_DEBUG ("Executing command: %s", command);
+  if (!g_spawn_command_line_sync (command, &stdout_data, NULL, &status, &error)
+      || !g_spawn_check_wait_status (status, &error))
   {
-    if (error)
-    {
-      g_warning ("failed to get value: %s", error->message);
-      g_error_free (error);
-    }
-    goto out;
+    g_warning ("Failed to get value: %s", error->message);
+    g_error_free (error);
+    g_free (command);
+    return FALSE;
   }
-  XFPM_DEBUG ("executed %s; retval: %i", command, exit_status);
-
-  if ( exit_status != 0 )
-    goto out;
 
 #if !defined(BACKEND_TYPE_FREEBSD)
   if ( stdout_data[0] == 'N' )
@@ -330,7 +322,6 @@ xfpm_brightness_helper_get_value (const gchar *argument)
   value = atoi (stdout_data);
 #endif
 
-out:
   g_free (command);
   g_free (stdout_data);
   return value;
@@ -383,28 +374,22 @@ xfpm_brightness_helper_get_level (XfpmBrightness *brg, gint32 *level)
 static gboolean
 xfpm_brightness_helper_set_level (XfpmBrightness *brg, gint32 level)
 {
-  gboolean ret;
   GError *error = NULL;
-  gint exit_status = 0;
-  gchar *command = NULL;
+  gint status;
+  gchar *command = g_strdup_printf ("pkexec " SBINDIR "/xfpm-power-backlight-helper --set-brightness %i", level);
 
-  command = g_strdup_printf ("pkexec " SBINDIR "/xfpm-power-backlight-helper --set-brightness %i", level);
-  ret = g_spawn_command_line_sync (command, NULL, NULL, &exit_status, &error);
-  if ( !ret )
+  XFPM_DEBUG ("Executing command: %s", command);
+  if (!g_spawn_command_line_sync (command, NULL, NULL, &status, &error)
+      || g_spawn_check_wait_status (status, &error))
   {
-    if (error)
-    {
-      g_warning ("xfpm_brightness_helper_set_level: failed to set value: %s", error->message);
-      g_error_free (error);
-    }
-    goto out;
+    g_warning ("Failed to set value: %s", error->message);
+    g_error_free (error);
+    g_free (command);
+    return FALSE;
   }
-  XFPM_DEBUG ("executed %s; retval: %i", command, exit_status);
-  ret = (exit_status == 0);
 
-out:
   g_free (command);
-  return ret;
+  return FALSE;
 }
 
 static gboolean
@@ -430,6 +415,7 @@ xfpm_brightness_helper_set_switch (XfpmBrightness *brg, gint brightness_switch)
   gint status;
   gchar *command = g_strdup_printf ("pkexec " SBINDIR "/xfpm-power-backlight-helper --set-brightness-switch %i", brightness_switch);
 
+  XFPM_DEBUG ("Executing command: %s", command);
   if (!g_spawn_command_line_sync (command, NULL, NULL, &status, &error)
       || !g_spawn_check_wait_status (status, &error))
   {

src/xfpm-suspend.c
@@ -109,32 +109,27 @@ freebsd_supports_sleep_state (const gchar *state)
 static gboolean
 linux_supports_sleep_state (const gchar *state)
 {
-  gboolean ret = FALSE;
   gchar *command;
   GError *error = NULL;
-  gint exit_status;
+  gint status;
 
   XFPM_DEBUG("entering");
 
   /* run script from pm-utils */
   command = g_strdup_printf ("/usr/bin/pm-is-supported --%s", state);
-  XFPM_DEBUG ("executing command: %s", command);
-  ret = g_spawn_command_line_sync (command, NULL, NULL, &exit_status, &error);
+  XFPM_DEBUG ("Executing command: %s", command);
 
-  if (!ret) {
-    if (error)
-    {
-      g_warning ("failed to run script: %s", error->message);
-      g_error_free (error);
-    }
-    goto out;
+  if (!g_spawn_command_line_sync (command, NULL, NULL, &status, &error)
+      || g_spawn_check_wait_status (status, &error))
+  {
+    g_warning ("Failed to run script: %s", error->message);
+    g_error_free (error);
+    g_free (command);
+    return FALSE;
   }
-  ret = (WIFEXITED(exit_status) && (WEXITSTATUS(exit_status) == EXIT_SUCCESS));
 
-out:
   g_free (command);
-
-  return ret;
+  return TRUE;
 }
 #endif
 
@@ -177,9 +172,8 @@ gboolean
 xfpm_suspend_try_action (XfpmActionType type)
 {
   const gchar *action;
-  gboolean ret;
   GError *error = NULL;
-  gint exit_status = 0;
+  gint status;
   gchar *command = NULL;
 
   TRACE("entering");
@@ -192,22 +186,17 @@ xfpm_suspend_try_action (XfpmActionType type)
     return FALSE;
 
   command = g_strdup_printf ("pkexec " SBINDIR "/xfce4-pm-helper --%s", action);
-  ret = g_spawn_command_line_sync (command, NULL, NULL, &exit_status, &error);
+  XFPM_DEBUG ("Executing command: %s", command);
 
-  if ( !ret )
+  if (!g_spawn_command_line_sync (command, NULL, NULL, &status, &error)
+      || !g_spawn_check_wait_status (status, &error))
   {
-    if (error)
-    {
-      g_warning ("xfce4-pm-helper: failed to suspend/hibernate: %s", error->message);
-      g_error_free (error);
-    }
-  }
-  else
-  {
-    XFPM_DEBUG ("executed %s; retval: %i", command, exit_status);
-    ret = (exit_status == 0);
+    g_warning ("Failed to suspend/hibernate: %s", error->message);
+    g_error_free (error);
+    g_free (command);
+    return FALSE;
   }
 
   g_free (command);
-  return ret;
+  return TRUE;
 }

commit d3b06c16ee92144342177b1dd801dc22ce42f888

Fix previous commit (again)
Fixes: 32519efa77e348af05b0abb3454c4fedd9e43077

common/xfpm-brightness.c
@@ -308,7 +308,7 @@ xfpm_brightness_helper_get_value (const gchar *argument)
     g_warning ("Failed to get value: %s", error->message);
     g_error_free (error);
     g_free (command);
-    return FALSE;
+    return value;
   }
 
 #if !defined(BACKEND_TYPE_FREEBSD)
@@ -389,7 +389,7 @@ xfpm_brightness_helper_set_level (XfpmBrightness *brg, gint32 level)
   }
 
   g_free (command);
-  return FALSE;
+  return TRUE;
 }
 
 static gboolean