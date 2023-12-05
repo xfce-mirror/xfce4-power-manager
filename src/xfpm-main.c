xfce-mirror/xfce4-power-manager

commit 15180f36b7e37a09f424372a1025b002ef193ec9

build: Restrict XfceSMClient to X11
Libxfce4ui >= 4.19.4 can be built without X11 support and therefore
whithout XfceSMClient.

src/xfpm-main.c
@@ -36,7 +36,9 @@
 #include <signal.h>
 
 #include <gtk/gtk.h>
-#include <glib.h>
+#ifdef ENABLE_X11
+#include <gdk/gdkx.h>
+#endif
 
 #include <libxfce4util/libxfce4util.h>
 #include <libxfce4ui/libxfce4ui.h>
@@ -282,7 +284,10 @@ int main (int argc, char **argv)
   octx = g_option_context_new("");
   g_option_context_set_ignore_unknown_options(octx, TRUE);
   g_option_context_add_main_entries(octx, option_entries, NULL);
-  g_option_context_add_group(octx, xfce_sm_client_get_option_group(argc, argv));
+#ifdef ENABLE_X11
+  if (GDK_IS_X11_DISPLAY (gdk_display_get_default ()))
+    g_option_context_add_group(octx, xfce_sm_client_get_option_group(argc, argv));
+#endif
   /* We can't add the following command because it will invoke gtk_init
      before we have a chance to fork.
      g_option_context_add_group(octx, gtk_get_option_group(TRUE));

src/xfpm-manager.c
@@ -93,7 +93,10 @@ struct XfpmManagerPrivate
   GDBusConnection    *session_bus;
   GDBusConnection    *system_bus;
 
+#ifdef ENABLE_X11
   XfceSMClient       *client;
+  gboolean            session_managed;
+#endif
 
   XfpmPower          *power;
   XfpmButton         *button;
@@ -116,7 +119,6 @@ struct XfpmManagerPrivate
   GTimer         *timer;
 
   gboolean          inhibited;
-  gboolean          session_managed;
 
   gint                inhibit_fd;
 };
@@ -179,7 +181,10 @@ xfpm_manager_finalize (GObject *object)
   g_object_unref (manager->priv->power);
   g_object_unref (manager->priv->button);
   g_object_unref (manager->priv->conf);
-  g_object_unref (manager->priv->client);
+#ifdef ENABLE_X11
+  if (manager->priv->client != NULL)
+    g_object_unref (manager->priv->client);
+#endif
   if ( manager->priv->systemd != NULL )
     g_object_unref (manager->priv->systemd);
   if ( manager->priv->console != NULL )
@@ -324,16 +329,20 @@ xfpm_manager_shutdown (XfpmManager *manager)
     g_warning ("Failed to shutdown the system : %s", error->message);
     g_error_free (error);
     /* Try with the session then */
+#ifdef ENABLE_X11
     if ( manager->priv->session_managed )
       xfce_sm_client_request_shutdown (manager->priv->client, XFCE_SM_CLIENT_SHUTDOWN_HINT_HALT);
+#endif
   }
 }
 
 static void
 xfpm_manager_ask_shutdown (XfpmManager *manager)
 {
+#ifdef ENABLE_X11
   if ( manager->priv->session_managed )
-  xfce_sm_client_request_shutdown (manager->priv->client, XFCE_SM_CLIENT_SHUTDOWN_HINT_ASK);
+    xfce_sm_client_request_shutdown (manager->priv->client, XFCE_SM_CLIENT_SHUTDOWN_HINT_ASK);
+#endif
 }
 
 static void
@@ -802,42 +811,43 @@ XfpmManager *
 xfpm_manager_new (GDBusConnection *bus, const gchar *client_id)
 {
   XfpmManager *manager = NULL;
-  GError *error = NULL;
-  gchar *current_dir;
-
-  const gchar *restart_command[] =
-  {
-    "xfce4-power-manager",
-    "--restart",
-    NULL
-  };
 
   manager = g_object_new (XFPM_TYPE_MANAGER, NULL);
 
   manager->priv->session_bus = bus;
 
-  current_dir = g_get_current_dir ();
-  manager->priv->client = xfce_sm_client_get_full (XFCE_SM_CLIENT_RESTART_NORMAL,
-                                                   XFCE_SM_CLIENT_PRIORITY_DEFAULT,
-                                                   client_id,
-                                                   current_dir,
-                                                   restart_command,
-                                                   SYSCONFDIR "/xdg/autostart/" PACKAGE_NAME ".desktop");
-
-  g_free (current_dir);
-
-  manager->priv->session_managed = xfce_sm_client_connect (manager->priv->client, &error);
-
-  if ( error )
+#ifdef ENABLE_X11
+  if (WINDOWING_IS_X11 ())
   {
-    g_warning ("Unable to connect to session manager : %s", error->message);
-    g_error_free (error);
+    GError *error = NULL;
+    gchar *current_dir = g_get_current_dir ();
+    const gchar *restart_command[] =
+    {
+      "xfce4-power-manager",
+      "--restart",
+      NULL
+    };
+    manager->priv->client = xfce_sm_client_get_full (XFCE_SM_CLIENT_RESTART_NORMAL,
+                                                     XFCE_SM_CLIENT_PRIORITY_DEFAULT,
+                                                     client_id,
+                                                     current_dir,
+                                                     restart_command,
+                                                     SYSCONFDIR "/xdg/autostart/" PACKAGE_NAME ".desktop");
+    g_free (current_dir);
+
+    manager->priv->session_managed = xfce_sm_client_connect (manager->priv->client, &error);
+    if (error != NULL)
+    {
+      g_warning ("Unable to connect to session manager : %s", error->message);
+      g_error_free (error);
+    }
+    else
+    {
+      g_signal_connect_object (manager->priv->client, "quit",
+                               G_CALLBACK (xfpm_manager_quit), manager, G_CONNECT_SWAPPED);
+    }
   }
-  else
-  {
-    g_signal_connect_object (manager->priv->client, "quit",
-                             G_CALLBACK (xfpm_manager_quit), manager, G_CONNECT_SWAPPED);
-  }
+#endif
 
   xfpm_manager_dbus_class_init (XFPM_MANAGER_GET_CLASS (manager));
   xfpm_manager_dbus_init (manager);

commit a826b11bca7c1f67d954e24ce7e5d4667642479e

Always add sm-client option group
It's too early to determine the windowing system at this stage, as we
haven't yet called gtk_init(). And if we do call it, we can no longer
daemonize below, as was mentioned in a comment about
gtk_get_option_group(). So the simplest thing to do is to always add
this option group (provided you're compiling with X11 support), which
activates very little code in libxfce4ui and nothing specific to X11.

Fixes: 15180f36b7e37a09f424372a1025b002ef193ec9

src/xfpm-main.c
@@ -285,8 +285,7 @@ int main (int argc, char **argv)
   g_option_context_set_ignore_unknown_options(octx, TRUE);
   g_option_context_add_main_entries(octx, option_entries, NULL);
 #ifdef ENABLE_X11
-  if (GDK_IS_X11_DISPLAY (gdk_display_get_default ()))
-    g_option_context_add_group(octx, xfce_sm_client_get_option_group(argc, argv));
+  g_option_context_add_group(octx, xfce_sm_client_get_option_group(argc, argv));
 #endif
   /* We can't add the following command because it will invoke gtk_init
      before we have a chance to fork.