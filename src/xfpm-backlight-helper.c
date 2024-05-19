xfce-mirror/xfce4-power-manager

commit f7903092e3de14408317778b66e9901e54a551ae

Clean up and sort includes

src/xfpm-backlight-helper.c
@@ -23,22 +23,10 @@
 #include "config.h"
 #endif
 
-#ifdef HAVE_UNISTD_H
-#include <unistd.h>
-#endif
-#include <glib-object.h>
-#ifdef HAVE_SYS_TYPES_H
-#include <sys/types.h>
-#endif
-#ifdef HAVE_SYS_STAT_H
-#include <sys/stat.h>
-#endif
 #include <fcntl.h>
-#ifdef HAVE_STRING_H
-#include <string.h>
-#endif
+#include <glib-object.h>
 #include <stdio.h>
-#include <stdlib.h>
+
 #if defined(BACKEND_TYPE_FREEBSD)
 #include <sys/sysctl.h>
 #endif

src/xfpm-pm-helper.c
@@ -24,31 +24,11 @@
  */
 
 #ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#ifdef HAVE_SYS_WAIT_H
-#include <sys/wait.h>
-#endif
-
-#ifdef HAVE_MEMORY_H
-#include <memory.h>
-#endif
-#ifdef HAVE_SIGNAL_H
-#include <signal.h>
-#endif
-#include <stdio.h>
-#ifdef HAVE_STDLIB_H
-#include <stdlib.h>
-#endif
-#ifdef HAVE_STRING_H
-#include <string.h>
-#endif
-#ifdef HAVE_UNISTD_H
-#include <unistd.h>
+#include "config.h"
 #endif
 
 #include <glib.h>
+#include <stdio.h>
 
 /* XXX */
 #define EXIT_CODE_SUCCESS           0

src/xfpm-polkit.c
@@ -19,32 +19,13 @@
  */
 
 #ifdef HAVE_CONFIG_H
-#include <config.h>
+#include "config.h"
 #endif
 
-#include <stdio.h>
-#ifdef HAVE_STDLIB_H
-#include <stdlib.h>
-#endif
-#ifdef HAVE_STRING_H
-#include <string.h>
-#endif
-#ifdef HAVE_UNISTD_H
-#include <unistd.h>
-#endif
+#include "xfpm-polkit.h"
 
-#ifdef HAVE_SYS_TYPES_H
-#include <sys/types.h>
-#endif
-#ifdef HAVE_SYS_PARAM_H
-#include <sys/param.h>
-#endif
-#ifdef HAVE_SYS_SYSCTL_H
-#include <sys/sysctl.h>
-#endif
-#ifdef HAVE_SYS_USER_H
-#include <sys/user.h>
-#endif
+#include "common/xfpm-common.h"
+#include "common/xfpm-debug.h"
 
 #if defined(__FreeBSD__)
 #include <sys/stat.h>
@@ -52,15 +33,6 @@
 #include <fcntl.h>
 #include <procfs.h>
 #endif
-
-#ifdef HAVE_ERRNO_H
-#include <errno.h>
-#endif
-
-#include "xfpm-polkit.h"
-#include "xfpm-debug.h"
-
-#include "xfpm-common.h"
 
 static void xfpm_polkit_finalize   (GObject *object);
 

src/xfpm-suspend.c
@@ -18,34 +18,15 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #ifdef HAVE_CONFIG_H
-#include <config.h>
+#include "config.h"
 #endif
 
-#include <stdio.h>
-#ifdef HAVE_STDLIB_H
-#include <stdlib.h>
-#endif
-#ifdef HAVE_STRING_H
-#include <string.h>
-#endif
-#ifdef HAVE_SYS_WAIT_H
-#include <sys/wait.h>
-#endif
-#ifdef HAVE_SYS_RESOURCE_H
-#include <sys/resource.h>
-#endif
-#ifdef HAVE_SYS_SYSCTL_H
-#include <sys/sysctl.h>
-#endif
-#ifdef HAVE_ERRNO_H
-#include <errno.h>
-#endif
+#include "xfpm-suspend.h"
+
+#include "common/xfpm-common.h"
+#include "common/xfpm-debug.h"
 
 #include <libxfce4util/libxfce4util.h>
-
-#include "xfpm-common.h"
-#include "xfpm-debug.h"
-#include "xfpm-suspend.h"
 
 
 

common/xfpm-brightness-polkit.c
@@ -20,7 +20,7 @@
  */
 
 #ifdef HAVE_CONFIG_H
-#include <config.h>
+#include "config.h"
 #endif
 
 #include "xfpm-brightness-polkit.h"

common/xfpm-brightness-x11.c
@@ -20,16 +20,16 @@
  */
 
 #ifdef HAVE_CONFIG_H
-#include <config.h>
+#include "config.h"
 #endif
-
-#include <gdk/gdkx.h>
-#include <X11/Xatom.h>
-#include <X11/Xlib.h>
-#include <X11/extensions/Xrandr.h>
 
 #include "xfpm-brightness-x11.h"
 #include "xfpm-debug.h"
+
+#include <X11/Xatom.h>
+#include <X11/Xlib.h>
+#include <X11/extensions/Xrandr.h>
+#include <gdk/gdkx.h>
 
 static gboolean       xfpm_brightness_x11_setup           (XfpmBrightness       *brightness,
                                                            gint32               *min_level,

common/xfpm-brightness.c
@@ -20,21 +20,22 @@
  */
 
 #ifdef HAVE_CONFIG_H
-#include <config.h>
+#include "config.h"
 #endif
-
-#include <math.h>
 
 #include "xfpm-brightness.h"
 #include "xfpm-debug.h"
 
 #ifdef ENABLE_X11
-#include <gdk/gdkx.h>
 #include "xfpm-brightness-x11.h"
+#include <gdk/gdkx.h>
 #endif
+
 #ifdef HAVE_POLKIT
 #include "xfpm-brightness-polkit.h"
 #endif
+
+#include <math.h>
 
 #define get_instance_private(instance) ((XfpmBrightnessPrivate *) \
   xfpm_brightness_get_instance_private (XFPM_BRIGHTNESS (instance)))

common/xfpm-common.c
@@ -19,15 +19,13 @@
  */
 
 #ifdef HAVE_CONFIG_H
-#include <config.h>
+#include "config.h"
 #endif
-
-#include <glib.h>
-
-#include <libxfce4util/libxfce4util.h>
 
 #include "xfpm-common.h"
 #include "xfpm-debug.h"
+
+#include <libxfce4util/libxfce4util.h>
 
 const gchar
 *xfpm_bool_to_string (gboolean value)

commit d4b043a9f2760324a9a63a304ef5c8611df1400b

build: Put standard headers back in where there's non-linux code
Fixes: f7903092e3de14408317778b66e9901e54a551ae

src/xfpm-backlight-helper.c
@@ -23,13 +23,24 @@
 #include "config.h"
 #endif
 
+#include <glib-object.h>
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
-
-#include <fcntl.h>
-#include <glib-object.h>
-#include <stdio.h>
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+#ifdef HAVE_SYS_STAT_H
+#include <sys/stat.h>
+#endif
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
 
 #ifdef BACKEND_TYPE_FREEBSD
 #include <sys/sysctl.h>

src/xfpm-pm-helper.c
@@ -27,15 +27,28 @@
 #include "config.h"
 #endif
 
+#include <glib.h>
+
+#include <stdio.h>
+
 #ifdef HAVE_SYS_WAIT_H
 #include <sys/wait.h>
+#endif
+#ifdef HAVE_MEMORY_H
+#include <memory.h>
+#endif
+#ifdef HAVE_SIGNAL_H
+#include <signal.h>
+#endif
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+#ifdef HAVE_STRING_H
+#include <string.h>
 #endif
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
-
-#include <glib.h>
-#include <stdio.h>
 
 /* XXX */
 #define EXIT_CODE_SUCCESS 0

src/xfpm-polkit.c
@@ -27,6 +27,33 @@
 #include "common/xfpm-common.h"
 #include "common/xfpm-debug.h"
 
+#include <stdio.h>
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+#ifdef HAVE_SYS_PARAM_H
+#include <sys/param.h>
+#endif
+#ifdef HAVE_SYS_SYSCTL_H
+#include <sys/sysctl.h>
+#endif
+#ifdef HAVE_SYS_USER_H
+#include <sys/user.h>
+#endif
+#ifdef HAVE_ERRNO_H
+#include <errno.h>
+#endif
+
 #if defined(__FreeBSD__)
 #include <sys/stat.h>
 #elif defined(__SVR4) && defined(__sun)

src/xfpm-suspend.c
@@ -28,6 +28,26 @@
 
 #include <libxfce4util/libxfce4util.h>
 
+#include <stdio.h>
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+#ifdef HAVE_SYS_WAIT_H
+#include <sys/wait.h>
+#endif
+#ifdef HAVE_SYS_RESOURCE_H
+#include <sys/resource.h>
+#endif
+#ifdef HAVE_SYS_SYSCTL_H
+#include <sys/sysctl.h>
+#endif
+#ifdef HAVE_ERRNO_H
+#include <errno.h>
+#endif
 
 
 #ifdef BACKEND_TYPE_FREEBSD