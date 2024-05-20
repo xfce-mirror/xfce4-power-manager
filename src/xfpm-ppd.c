xfce-mirror/xfce4-power-manager

commit 027098d4bdf4693a3b095e5a7834f88b27051dca

Add support for power profiles daemon

src/xfpm-ppd.c
@@ -1,0 +1,223 @@
+/*
+ * * Copyright (C) 2023 Elliot <BlindRepublic@mailo.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <gio/gio.h>
+
+#include "xfpm-config.h"
+#include "xfpm-xfconf.h"
+#include "xfpm-power.h"
+#include "xfpm-ppd-common.h"
+#include "xfpm-ppd.h"
+#include "xfpm-debug.h"
+
+static void xfpm_ppd_finalize (GObject *object);
+
+static void xfpm_ppd_get_property (GObject *object,
+                                   guint prop_id,
+                                   GValue *value,
+                                   GParamSpec *pspec);
+
+static void xfpm_ppd_set_property (GObject *object,
+                                   guint prop_id,
+                                   const GValue *value,
+                                   GParamSpec *pspec);
+
+struct _XfpmPPD
+{
+  GObject parent;
+
+  XfpmXfconf *conf;
+  XfpmPower *power;
+
+  GDBusProxy *proxy;
+
+  gchar *profile_on_ac;
+  gchar *profile_on_battery;
+};
+
+enum
+{
+  PROP_0,
+  PROP_PROFILE_ON_AC,
+  PROP_PROFILE_ON_BATTERY,
+  N_PROPERTIES
+};
+
+G_DEFINE_FINAL_TYPE (XfpmPPD, xfpm_ppd, G_TYPE_OBJECT)
+
+static void
+xfpm_ppd_set_active_profile (XfpmPPD *ppd, const gchar *profile)
+{
+  GVariant *var = NULL;
+  GError *error = NULL;
+
+  g_return_if_fail (XFPM_IS_PPD (ppd));
+  g_return_if_fail (profile != NULL);
+
+  var = g_dbus_proxy_call_sync (ppd->proxy,
+                                "org.freedesktop.DBus.Properties.Set",
+                                g_variant_new ("(ssv)", "net.hadess.PowerProfiles", "ActiveProfile",
+                                               g_variant_new_string (profile)),
+                                G_DBUS_CALL_FLAGS_NONE,
+                                -1, NULL, &error);
+
+  if (var != NULL)
+    g_variant_unref (var);
+
+  if (error != NULL)
+  {
+    XFPM_DEBUG ("Failed to set active power profile : %s", error->message);
+    g_error_free (error);
+  }
+}
+
+static void
+xfpm_ppd_on_battery_changed (XfpmPower *power, gboolean on_battery, XfpmPPD *ppd)
+{
+  xfpm_ppd_set_active_profile (ppd, on_battery ? ppd->profile_on_battery : ppd->profile_on_ac);
+}
+
+static void
+xfpm_ppd_class_init (XfpmPPDClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->finalize = xfpm_ppd_finalize;
+
+  object_class->get_property = xfpm_ppd_get_property;
+  object_class->set_property = xfpm_ppd_set_property;
+
+  g_object_class_install_property (object_class,
+                                   PROP_PROFILE_ON_AC,
+                                   g_param_spec_string ("profile-on-ac",
+                                                        NULL, NULL,
+                                                        NULL,
+                                                        G_PARAM_READWRITE));
+
+  g_object_class_install_property (object_class,
+                                   PROP_PROFILE_ON_BATTERY,
+                                   g_param_spec_string ("profile-on-battery",
+                                                        NULL, NULL,
+                                                        NULL,
+                                                        G_PARAM_READWRITE));
+}
+
+static void
+xfpm_ppd_init (XfpmPPD *ppd)
+{
+  if ((ppd->proxy = xfpm_ppd_g_dbus_proxy_new ()) == NULL)
+    return;
+
+  ppd->conf = xfpm_xfconf_new ();
+  ppd->power = xfpm_power_get ();
+
+  xfconf_g_property_bind (xfpm_xfconf_get_channel (ppd->conf),
+                          XFPM_PROPERTIES_PREFIX PROFILE_ON_AC, G_TYPE_STRING,
+                          G_OBJECT (ppd), PROFILE_ON_AC);
+
+  xfconf_g_property_bind (xfpm_xfconf_get_channel (ppd->conf),
+                          XFPM_PROPERTIES_PREFIX PROFILE_ON_BATTERY, G_TYPE_STRING,
+                          G_OBJECT (ppd), PROFILE_ON_BATTERY);
+
+  g_signal_connect (ppd->power, "on-battery-changed", G_CALLBACK (xfpm_ppd_on_battery_changed), ppd);
+}
+
+static void
+xfpm_ppd_get_property (GObject *object,
+                       guint prop_id,
+                       GValue *value,
+                       GParamSpec *pspec)
+{
+  XfpmPPD *ppd = XFPM_PPD (object);
+
+  switch (prop_id)
+  {
+    case PROP_PROFILE_ON_AC:
+      g_value_set_string (value, ppd->profile_on_ac);
+      break;
+    case PROP_PROFILE_ON_BATTERY:
+      g_value_set_string (value, ppd->profile_on_battery);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static void
+xfpm_ppd_set_property (GObject *object,
+                       guint prop_id,
+                       const GValue *value,
+                       GParamSpec *pspec)
+{
+  XfpmPPD *ppd = XFPM_PPD (object);
+  gboolean on_battery;
+
+  /* Modify property and apply profile if necessary */
+  g_object_get (ppd->power, "on-battery", &on_battery, NULL);
+
+  switch (prop_id)
+  {
+    case PROP_PROFILE_ON_AC:
+      g_free (ppd->profile_on_ac);
+      ppd->profile_on_ac = g_value_dup_string (value);
+      if (!on_battery)
+        xfpm_ppd_set_active_profile (ppd, ppd->profile_on_ac);
+      break;
+    case PROP_PROFILE_ON_BATTERY:
+      g_free (ppd->profile_on_battery);
+      ppd->profile_on_battery = g_value_dup_string (value);
+      if (on_battery)
+        xfpm_ppd_set_active_profile (ppd, ppd->profile_on_battery);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static void
+xfpm_ppd_finalize (GObject *object)
+{
+  XfpmPPD *ppd = XFPM_PPD (object);
+
+  if (ppd->conf != NULL)
+    g_object_unref (ppd->conf);
+
+  if (ppd->power != NULL)
+  {
+    g_signal_handlers_disconnect_by_func (ppd->power, xfpm_ppd_on_battery_changed, ppd);
+    g_object_unref (ppd->power);
+  }
+
+  if (ppd->proxy != NULL)
+    g_object_unref (ppd->proxy);
+
+  g_free (ppd->profile_on_ac);
+  g_free (ppd->profile_on_battery);
+
+  G_OBJECT_CLASS (xfpm_ppd_parent_class)->finalize (object);
+}
+
+XfpmPPD *
+xfpm_ppd_new (void)
+{
+  return g_object_new (XFPM_TYPE_PPD, NULL);
+}

common/Makefile.am
@@ -12,6 +12,8 @@ libxfpmcommon_la_SOURCES =  \
 	xfpm-icons.h            \
 	xfpm-power-common.c     \
 	xfpm-power-common.h     \
+	xfpm-ppd-common.c       \
+	xfpm-ppd-common.h       \
 	xfpm-enum.h             \
 	xfpm-enum-glib.h
 

common/xfpm-config.h
@@ -47,6 +47,9 @@ G_BEGIN_DECLS
 #define ON_BATT_DPMS_OFF                     "dpms-on-battery-off"
 #define DPMS_SLEEP_MODE                      "dpms-sleep-mode"
 
+#define PROFILE_ON_AC                        "profile-on-ac"
+#define PROFILE_ON_BATTERY                   "profile-on-battery"
+
 #define LOCK_SCREEN_ON_SLEEP                 "lock-screen-suspend-hibernate"
 #define GENERAL_NOTIFICATION_CFG             "general-notification"
 #define PRESENTATION_MODE                    "presentation-mode"

common/xfpm-ppd-common.c
@@ -1,0 +1,80 @@
+/*
+ * * Copyright (C) 2023 Elliot <BlindRepublic@mailo.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <gio/gio.h>
+
+#include "xfpm-ppd-common.h"
+
+GDBusProxy *
+xfpm_ppd_g_dbus_proxy_new (void)
+{
+  GError *error = NULL;
+
+  /* Helper function to create a dbus proxy for the power profiles daemon*/
+  GDBusProxy *proxy = g_dbus_proxy_new_for_bus_sync (G_BUS_TYPE_SYSTEM,
+                                                     G_DBUS_PROXY_FLAGS_NONE,
+                                                     NULL,
+                                                     "net.hadess.PowerProfiles",
+                                                     "/net/hadess/PowerProfiles",
+                                                     "net.hadess.PowerProfiles",
+                                                     NULL, &error);
+
+  if (error != NULL)
+  {
+    g_warning ("Unable to get the interface, net.hadess.PowerProfiles : %s", error->message);
+    g_error_free (error);
+  }
+
+  return proxy;
+}
+
+GSList *
+xfpm_ppd_get_profiles (GDBusProxy *proxy)
+{
+  GVariant *var  = NULL;
+  GSList *names = NULL;
+
+  g_return_val_if_fail (proxy != NULL, NULL);
+
+  var = g_dbus_proxy_get_cached_property (proxy, "Profiles");
+
+  if (var != NULL)
+  {
+    GVariantIter iter;
+    GVariant *profile;
+
+    /* Iterate over the profile dictionary */
+    g_variant_iter_init (&iter, var);
+    while ((profile = g_variant_iter_next_value (&iter)) != NULL)
+    {
+      gchar *name;
+
+      /* Get the profile name */
+      if (g_variant_lookup (profile, "Profile", "s", &name))
+        names = g_slist_append (names, name);
+
+      g_variant_unref (profile);
+    }
+
+    g_variant_unref (var);
+  }
+
+  return names;
+}

common/xfpm-ppd-common.h
@@ -1,0 +1,33 @@
+/*
+ * * Copyright (C) 2023 Elliot <BlindRepublic@mailo.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef __XFPM_PPD_COMMON_H
+#define __XFPM_PPD_COMMON_H
+
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+
+GDBusProxy            *xfpm_ppd_g_dbus_proxy_new   (void);
+GSList                *xfpm_ppd_get_profiles       (GDBusProxy *proxy);
+
+G_END_DECLS
+
+#endif

configure.ac
@@ -58,7 +58,7 @@ AM_GNU_GETTEXT_VERSION([0.19.8])
 #          Check for required packages 		      #
 #=====================================================#
 m4_define([gtk_minimum_version], [3.24.0])
-m4_define([glib_minimum_version], [2.66.0])
+m4_define([glib_minimum_version], [2.70.0])
 
 m4_define([xfconf_minimum_version], [4.12.0])
 m4_define([libxfce4ui_minimum_version],[4.18.4])

data/interfaces/xfpm-settings.ui
@@ -862,6 +862,39 @@
                                 <property name="top_attach">4</property>
                               </packing>
                             </child>
+                            <child>
+                              <object class="GtkLabel" id="power-profile-label-on-battery">
+                                <property name="visible">True</property>
+                                <property name="can_focus">False</property>
+                                <property name="label" translatable="yes">&lt;b&gt;Power profile&lt;/b&gt;</property>a
+                                <property name="use_markup">True</property>
+                                <property name="xalign">0</property>
+                              </object>
+                              <packing>
+                                <property name="left_attach">0</property>
+                                <property name="top_attach">5</property>
+                                <property name="width">2</property>
+                              </packing>
+                            </child>
+                            <child>
+                              <object class="GtkComboBox" id="power-profile-on-battery">
+                                <property name="visible">True</property>
+                                <property name="can_focus">False</property>
+                                <property name="hexpand">True</property>
+                                <signal name="changed" handler="on_battery_power_profile_changed_cb" swapped="no"/>
+                                <child>
+                                  <object class="GtkCellRendererText" id="renderer12"/>
+                                  <attributes>
+                                    <attribute name="text">0</attribute>
+                                  </attributes>
+                                </child>
+                              </object>
+                              <packing>
+                                <property name="left_attach">0</property>
+                                <property name="top_attach">6</property>
+                                <property name="width">2</property>
+                              </packing>
+                            </child>
                           </object>
                           <packing>
                             <property name="name">page0</property>
@@ -1018,6 +1051,39 @@
                               <packing>
                                 <property name="left_attach">0</property>
                                 <property name="top_attach">0</property>
+                                <property name="width">2</property>
+                              </packing>
+                            </child>
+                            <child>
+                              <object class="GtkLabel" id="power-profile-label-on-ac">
+                                <property name="visible">True</property>
+                                <property name="can_focus">False</property>
+                                <property name="label" translatable="yes">&lt;b&gt;Power profile&lt;/b&gt;</property>a
+                                <property name="use_markup">True</property>
+                                <property name="xalign">0</property>
+                              </object>
+                              <packing>
+                                <property name="left_attach">0</property>
+                                <property name="top_attach">5</property>
+                                <property name="width">2</property>
+                              </packing>
+                            </child>
+                            <child>
+                              <object class="GtkComboBox" id="power-profile-on-ac">
+                                <property name="visible">True</property>
+                                <property name="can_focus">False</property>
+                                <property name="hexpand">True</property>
+                                <signal name="changed" handler="on_ac_power_profile_changed_cb" swapped="no"/>
+                                <child>
+                                  <object class="GtkCellRendererText" id="renderer6"/>
+                                  <attributes>
+                                    <attribute name="text">0</attribute>
+                                  </attributes>
+                                </child>
+                              </object>
+                              <packing>
+                                <property name="left_attach">0</property>
+                                <property name="top_attach">6</property>
                                 <property name="width">2</property>
                               </packing>
                             </child>

settings/xfpm-settings.c
@@ -42,6 +42,7 @@
 #include "xfpm-debug.h"
 #include "xfpm-power-common.h"
 #include "xfpm-power.h"
+#include "xfpm-ppd-common.h"
 #include "xfpm-backlight.h"
 
 #include "interfaces/xfpm-settings_ui.h"
@@ -175,6 +176,10 @@ static void view_cursor_changed_cb                     (GtkTreeView *view,
 void        on_ac_sleep_mode_changed_cb                (GtkWidget *w,
                                                         XfconfChannel *channel);
 void        on_battery_sleep_mode_changed_cb           (GtkWidget *w,
+                                                        XfconfChannel *channel);
+void        on_ac_power_profile_changed_cb             (GtkWidget *w,
+                                                        XfconfChannel *channel);
+void        on_battery_power_profile_changed_cb        (GtkWidget *w,
                                                         XfconfChannel *channel);
 gboolean    handle_brightness_keys_toggled_cb          (GtkWidget *w,
                                                         gboolean is_active,
@@ -419,6 +424,59 @@ on_battery_sleep_mode_changed_cb (GtkWidget *w, XfconfChannel *channel)
     g_critical ("Cannot set value for property %s\n", INACTIVITY_SLEEP_MODE_ON_BATTERY);
   }
 }
+
+void
+on_ac_power_profile_changed_cb (GtkWidget *w, XfconfChannel *channel)
+{
+  GtkTreeModel *model;
+  GtkTreeIter selected_row;
+  gchar *profile = NULL;
+
+  if (!gtk_combo_box_get_active_iter (GTK_COMBO_BOX (w), &selected_row))
+    return;
+
+  model = gtk_combo_box_get_model (GTK_COMBO_BOX (w));
+
+  gtk_tree_model_get(model,
+                     &selected_row,
+                     0,
+                     &profile,
+                     -1);
+
+  if (!xfconf_channel_set_string (channel, XFPM_PROPERTIES_PREFIX PROFILE_ON_AC, profile))
+  {
+    g_critical ("Cannot set value for property %s\n", PROFILE_ON_AC);
+  }
+
+  g_free (profile);
+}
+
+void
+on_battery_power_profile_changed_cb (GtkWidget *w, XfconfChannel *channel)
+{
+  GtkTreeModel *model;
+  GtkTreeIter selected_row;
+  gchar *profile = NULL;
+
+  if (!gtk_combo_box_get_active_iter (GTK_COMBO_BOX (w), &selected_row))
+    return;
+
+  model = gtk_combo_box_get_model (GTK_COMBO_BOX (w));
+
+  gtk_tree_model_get(model,
+                     &selected_row,
+                     0,
+                     &profile,
+                     -1);
+
+  if (!xfconf_channel_set_string (channel, XFPM_PROPERTIES_PREFIX PROFILE_ON_BATTERY, profile))
+  {
+    g_critical ("Cannot set value for property %s\n", PROFILE_ON_BATTERY);
+  }
+
+  g_free (profile);
+}
+
 
 gboolean
 dpms_toggled_cb (GtkWidget *w, gboolean is_active, XfconfChannel *channel)
@@ -898,10 +956,11 @@ xfpm_update_logind_handle_lid_switch (XfconfChannel *channel)
 /* END Light Locker Integration */
 
 static void
-xfpm_settings_on_battery (XfconfChannel *channel, gboolean auth_suspend,
-                          gboolean auth_hibernate, gboolean can_suspend,
-                          gboolean can_hibernate, gboolean can_shutdown,
-                          gboolean has_lcd_brightness, gboolean has_lid)
+xfpm_settings_on_battery (XfconfChannel *channel, GDBusProxy *profiles_proxy,
+                          gboolean auth_suspend, gboolean auth_hibernate,
+                          gboolean can_suspend, gboolean can_hibernate,
+                          gboolean can_shutdown, gboolean has_lcd_brightness,
+                          gboolean has_lid)
 {
   gboolean valid, handle_dpms;
   gint list_value;
@@ -913,6 +972,8 @@ xfpm_settings_on_battery (XfconfChannel *channel, gboolean auth_suspend,
   GtkWidget *lid;
   GtkWidget *brg;
   GtkWidget *brg_level;
+  GtkWidget *power_profile;
+  GSList *profiles;
 
   /*
    * Inactivity sleep mode on battery
@@ -1105,6 +1166,55 @@ xfpm_settings_on_battery (XfconfChannel *channel, gboolean auth_suspend,
   }
 
   /*
+   * Power profile on battery
+   */
+  power_profile = GTK_WIDGET (gtk_builder_get_object (xml, "power-profile-on-battery"));
+  if (profiles_proxy != NULL && (profiles = xfpm_ppd_get_profiles (profiles_proxy)) != NULL)
+  {
+    gchar *enabled_profile;
+
+    list_store = gtk_list_store_new (1, G_TYPE_STRING);
+    gtk_combo_box_set_model (GTK_COMBO_BOX (power_profile), GTK_TREE_MODEL (list_store));
+
+    for (GSList *l = profiles; l != NULL; l = l->next)
+    {
+      gchar *profile = l->data;
+
+      gtk_list_store_append (list_store, &iter);
+      gtk_list_store_set (list_store, &iter, 0, profile, -1);
+    }
+
+    enabled_profile = xfconf_channel_get_string (channel, XFPM_PROPERTIES_PREFIX PROFILE_ON_BATTERY, "balanced");
+
+    for (valid = gtk_tree_model_get_iter_first (GTK_TREE_MODEL (list_store), &iter);
+         valid;
+         valid = gtk_tree_model_iter_next (GTK_TREE_MODEL (list_store), &iter))
+    {
+      gchar *profile_value;
+
+      gtk_tree_model_get (GTK_TREE_MODEL (list_store), &iter,
+                          0, &profile_value, -1);
+      if (g_strcmp0 (profile_value, enabled_profile) == 0)
+      {
+        gtk_combo_box_set_active_iter (GTK_COMBO_BOX (power_profile), &iter);
+        break;
+      }
+
+      g_free (profile_value);
+    }
+
+    g_free (enabled_profile);
+    g_slist_free_full (profiles, g_free);
+  }
+  else
+  {
+    gtk_widget_hide (GTK_WIDGET (gtk_builder_get_object (xml, "power-profile-label-on-battery")));
+    gtk_widget_hide (power_profile);
+  }
+
+
+
+  /*
    * Brightness on battery
    */
   brg = GTK_WIDGET (gtk_builder_get_object (xml ,"brightness-inactivity-on-battery"));
@@ -1139,20 +1249,22 @@ xfpm_settings_on_battery (XfconfChannel *channel, gboolean auth_suspend,
 }
 
 static void
-xfpm_settings_on_ac (XfconfChannel *channel, gboolean auth_suspend,
-                     gboolean auth_hibernate, gboolean can_suspend,
-                     gboolean can_hibernate, gboolean has_lcd_brightness,
-                     gboolean has_lid)
+xfpm_settings_on_ac (XfconfChannel *channel, GDBusProxy *profiles_proxy,
+                     gboolean auth_suspend, gboolean auth_hibernate,
+                     gboolean can_suspend, gboolean can_hibernate,
+                     gboolean has_lcd_brightness, gboolean has_lid)
 {
   gboolean valid, handle_dpms;
   GtkWidget *inact_timeout, *inact_action;
   GtkWidget *lid;
   GtkWidget *brg;
   GtkWidget *brg_level;
+  GtkWidget *power_profile;
   GtkListStore *list_store;
   GtkTreeIter iter;
   guint val;
   guint list_value;
+  GSList *profiles;
 
   /*
    * Inactivity sleep mode on AC
@@ -1292,6 +1404,53 @@ xfpm_settings_on_ac (XfconfChannel *channel, gboolean auth_suspend,
     gtk_widget_hide (GTK_WIDGET (gtk_builder_get_object (xml, "lid-action-label1")));
     gtk_widget_hide (lid);
     gtk_widget_hide (GTK_WIDGET (gtk_builder_get_object (xml, "lid-plugged-in-header")));
+  }
+
+  /*
+   * Power profile on AC power
+   */
+  power_profile = GTK_WIDGET (gtk_builder_get_object (xml, "power-profile-on-ac"));
+  if (profiles_proxy != NULL && (profiles = xfpm_ppd_get_profiles (profiles_proxy)) != NULL)
+  {
+    gchar *enabled_profile;
+
+    list_store = gtk_list_store_new (1, G_TYPE_STRING);
+    gtk_combo_box_set_model (GTK_COMBO_BOX (power_profile), GTK_TREE_MODEL (list_store));
+
+    for (GSList *l = profiles; l != NULL; l = l->next)
+    {
+      gchar *profile = l->data;
+
+      gtk_list_store_append (list_store, &iter);
+      gtk_list_store_set (list_store, &iter, 0, profile, -1);
+    }
+
+    enabled_profile = xfconf_channel_get_string (channel, XFPM_PROPERTIES_PREFIX PROFILE_ON_AC, "balanced");
+
+    for (valid = gtk_tree_model_get_iter_first (GTK_TREE_MODEL (list_store), &iter);
+         valid;
+         valid = gtk_tree_model_iter_next (GTK_TREE_MODEL (list_store), &iter))
+    {
+      gchar *profile_value;
+
+      gtk_tree_model_get (GTK_TREE_MODEL (list_store), &iter,
+                          0, &profile_value, -1);
+      if (g_strcmp0 (profile_value, enabled_profile) == 0)
+      {
+        gtk_combo_box_set_active_iter (GTK_COMBO_BOX (power_profile), &iter);
+        break;
+      }
+
+      g_free (profile_value);
+    }
+
+    g_free (enabled_profile);
+    g_slist_free_full (profiles, g_free);
+  }
+  else
+  {
+    gtk_widget_hide (GTK_WIDGET (gtk_builder_get_object (xml, "power-profile-label-on-ac")));
+    gtk_widget_hide (power_profile);
   }
 
   /*
@@ -2437,6 +2596,7 @@ xfpm_settings_dialog_new (XfconfChannel *channel, gboolean auth_suspend,
   GError *error = NULL;
   guint val;
   GtkCssProvider *css_provider;
+  GDBusProxy *profiles_proxy = xfpm_ppd_g_dbus_proxy_new ();
 
   XFPM_DEBUG ("auth_hibernate=%s auth_suspend=%s can_shutdown=%s can_suspend=%s can_hibernate=%s " \
               "has_battery=%s has_lcd_brightness=%s has_lid=%s has_sleep_button=%s " \
@@ -2555,6 +2715,7 @@ xfpm_settings_dialog_new (XfconfChannel *channel, gboolean auth_suspend,
   settings_create_devices_list ();
 
   xfpm_settings_on_ac (channel,
+                       profiles_proxy,
                        auth_suspend,
                        auth_hibernate,
                        can_suspend,
@@ -2562,8 +2723,9 @@ xfpm_settings_dialog_new (XfconfChannel *channel, gboolean auth_suspend,
                        has_lcd_brightness,
                        has_lid);
 
-  if ( has_battery )
+  if (has_battery)
   xfpm_settings_on_battery (channel,
+                            profiles_proxy,
                             auth_suspend,
                             auth_hibernate,
                             can_suspend,
@@ -2642,6 +2804,9 @@ xfpm_settings_dialog_new (XfconfChannel *channel, gboolean auth_suspend,
   /* keep a pointer to the GtkApplication instance so we can signal a
    * quit message */
   app = gtk_app;
+
+  if (profiles_proxy != NULL)
+    g_object_unref (profiles_proxy);
 
   return dialog;
 }

commit 8f2717652467504a6065757990b72bd840035392

ppd: Add missing profile initialization
So there is no critical warning when switching between ac and battery
with no profile set. We can't use G_PARAM_CONSTRUCT here since we bind
the property in init (not in constructed), so the default gobject value
would override the settings.

Fixes: 027098d4bdf4693a3b095e5a7834f88b27051dca

src/xfpm-ppd.c
@@ -137,6 +137,8 @@ xfpm_ppd_init (XfpmPPD *ppd)
 
   ppd->conf = xfpm_xfconf_new ();
   ppd->power = xfpm_power_get ();
+  ppd->profile_on_ac = g_strdup (DEFAULT_PROFILE_ON_AC);
+  ppd->profile_on_battery = g_strdup (DEFAULT_PROFILE_ON_BATTERY);
 
   xfconf_g_property_bind (xfpm_xfconf_get_channel (ppd->conf),
                           XFPM_PROPERTIES_PREFIX PROFILE_ON_AC, G_TYPE_STRING,