xfce-mirror/xfce4-power-manager

commit 4fb67837cc2c4e3a8196128ed8086158f4ca149a

Remove outdated references to xfce-goodies

src/xfpm-main.c
@@ -40,13 +40,7 @@
 static void G_GNUC_NORETURN
 show_version (void)
 {
-  g_print (_("\n"
-           "Xfce Power Manager %s\n\n"
-           "Part of the Xfce Goodies Project\n"
-           "http://goodies.xfce.org\n\n"
-           "Licensed under the GNU GPL.\n\n"),
-           VERSION);
-
+  g_print ("Xfce Power Manager %s", VERSION);
   exit (EXIT_SUCCESS);
 }
 

settings/xfce4-power-manager-settings.1
@@ -47,7 +47,7 @@ mailing list. See:
 .IR https://mail.xfce.org/mailman/listinfo/
 
 .SH HOMEPAGE
-.I http://goodies.xfce.org/
+.I https://docs.xfce.org/xfce/xfce4-power-manager/start
 
 .SH AUTHOR
 Ali Abdallah <aliov@xfce.org>,

src/org.xfce.power.policy.in.in
@@ -10,7 +10,7 @@
   -->
 
   <vendor>Xfce Power Manager</vendor>
-  <vendor_url>http://goodies.xfce.org/projects/applications/xfce4-power-manager</vendor_url>
+  <vendor_url>https://docs.xfce.org/xfce/xfce4-power-manager/start</vendor_url>
   <icon_name>battery</icon_name>
 
   <action id="org.xfce.power.backlight-helper">
@@ -44,4 +44,3 @@
   </action>
 
 </policyconfig>
-

src/xfce4-power-manager.1
@@ -58,7 +58,7 @@ mailing list. See:
 .IR https://mail.xfce.org/mailman/listinfo/
 
 .SH HOMEPAGE
-.I http://goodies.xfce.org/
+.I https://docs.xfce.org/xfce/xfce4-power-manager/start
 
 .SH AUTHOR
 Ali Abdallah <aliov@xfce.org>,

src/xfpm-manager.c
@@ -1142,6 +1142,6 @@ xfpm_manager_dbus_get_info (XfpmManager *manager,
                             GDBusMethodInvocation *invocation,
                             gpointer user_data)
 {
-  xfpm_power_manager_complete_get_info (user_data, invocation, PACKAGE, VERSION, "Xfce-goodies");
+  xfpm_power_manager_complete_get_info (user_data, invocation, PACKAGE, VERSION, "Xfce");
   return TRUE;
 }

commit aad6179ded667fc603afd1c423b3a5b27a11c0f3

main: Add missing newline in version info
Fixes: 4fb67837cc2c4e3a8196128ed8086158f4ca149a

src/xfpm-main.c
@@ -40,7 +40,7 @@
 static void G_GNUC_NORETURN
 show_version (void)
 {
-  g_print ("Xfce Power Manager %s", VERSION);
+  g_print ("Xfce Power Manager %s\n", VERSION);
   exit (EXIT_SUCCESS);
 }
 