d6b211d9657859f13a60c6082732b4f752024c76
Gaël Bonithon <gael@xfce.org>
2024-05-24 23:40:55 +0200

    settings: Rework starting xfpm if not already running
    Trying to retrieve the configuration is not a valid test of xfpm's
    presence on dbus, it can fail for other reasons, and this loop with
    g_usleep() is not good. Better to listen to
    GDBusProxy::notify::g-name-owner, with a timeout. Also, there's no point
    in continuing if xfconf can't be initialized, besides the xfconf API is
    used unconditionally afterwards.

--- settings/xfpm-settings-app.c
+++ settings/xfpm-settings-app.c
@@ -45,6 +45,8 @@ struct _XfpmSettingsAppPrivate
   Window socket_id;
 #endif
   gchar *device_id;
+  guint abort_id;
+  XfpmPowerManager *manager;
 };
 
 static void
@@ -116,11 +118,39 @@ xfpm_settings_app_activate (GApplication *app)
 }
 
 static void
-xfpm_settings_app_launch (GApplication *app)
+xfpm_settings_app_shutdown (GApplication *app)
 {
   XfpmSettingsAppPrivate *priv = xfpm_settings_app_get_instance_private (XFPM_SETTINGS_APP (app));
 
-  XfpmPowerManager *manager;
+  if (priv->abort_id != 0)
+    g_source_remove (priv->abort_id);
+  if (priv->manager != NULL)
+    g_object_unref (priv->manager);
+
+  G_APPLICATION_CLASS (xfpm_settings_app_parent_class)->shutdown (app);
+}
+
+static gboolean
+abort_xfpm_startup (gpointer data)
+{
+  XfpmSettingsApp *app = data;
+  XfpmSettingsAppPrivate *priv = xfpm_settings_app_get_instance_private (app);
+
+  g_critical ("Failed to start xfce4-power-manager, timeout reached");
+  xfce_dialog_show_warning (NULL, _("Xfce Power Manager"),
+                            "%s", _("Failed to connect to power manager"));
+  g_application_release (G_APPLICATION (app));
+  priv->abort_id = 0;
+
+  return FALSE;
+}
+
+static void
+get_config (XfpmPowerManager *manager,
+            GParamSpec *pspec,
+            XfpmSettingsApp *app)
+{
+  XfpmSettingsAppPrivate *priv = xfpm_settings_app_get_instance_private (app);
   XfconfChannel *channel;
   GError *error = NULL;
   GtkWidget *dialog;
@@ -128,8 +158,6 @@ xfpm_settings_app_launch (GApplication *app)
   GVariant *config;
   GVariantIter *iter;
   gchar *key, *value;
-  GList *windows;
-
   gboolean has_battery;
   gboolean auth_suspend;
   gboolean auth_hibernate;
@@ -144,90 +172,25 @@ xfpm_settings_app_launch (GApplication *app)
   gboolean has_power_button;
   gboolean has_battery_button;
   gboolean has_lid;
-  gint start_xfpm_if_not_running;
 
-  windows = gtk_application_get_windows (GTK_APPLICATION (app));
-
-  if (windows != NULL)
+  g_signal_handlers_disconnect_by_func (manager, get_config, app);
+  g_application_release (G_APPLICATION (app));
+  if (priv->abort_id != 0)
   {
-    XFPM_DEBUG ("window already opened");
-
-    gtk_window_present (windows->data);
-    gdk_notify_startup_complete ();
-
-    if (priv->device_id != NULL)
-    {
-      xfpm_settings_show_device_id (priv->device_id);
-    }
-
-    return;
+    g_source_remove (priv->abort_id);
+    priv->abort_id = 0;
   }
 
-  manager = xfpm_power_manager_proxy_new_for_bus_sync (G_BUS_TYPE_SESSION,
-                                                       G_DBUS_PROXY_FLAGS_NONE,
-                                                       "org.xfce.PowerManager",
-                                                       "/org/xfce/PowerManager",
-                                                       NULL,
-                                                       &error);
-
-  if (error != NULL)
+  if (!xfpm_power_manager_call_get_config_sync (manager, &config, NULL, &error))
   {
-    g_critical ("xfpm_power_manager_proxy_new_sync failed: %s", error->message);
+    g_critical ("xfpm_power_manager_call_get_config_sync failed: %s", error->message);
     xfce_dialog_show_warning (NULL, _("Xfce Power Manager"),
                               "%s", _("Failed to connect to power manager"));
-    g_clear_error (&error);
+    g_error_free (error);
     return;
   }
 
-
-  while (!xfpm_power_manager_call_get_config_sync (manager, &config, NULL, NULL))
-  {
-    GtkWidget *startw;
-
-    startw = gtk_message_dialog_new (NULL,
-                                     GTK_DIALOG_MODAL,
-                                     GTK_MESSAGE_QUESTION,
-                                     GTK_BUTTONS_YES_NO,
-                                     _("Xfce4 Power Manager is not running, do you want to launch it now?"));
-    start_xfpm_if_not_running = gtk_dialog_run (GTK_DIALOG (startw));
-    gtk_widget_destroy (startw);
-
-    if (start_xfpm_if_not_running == GTK_RESPONSE_YES)
-    {
-      GAppInfo *app_info;
-
-      app_info = g_app_info_create_from_commandline ("xfce4-power-manager", "Xfce4 Power Manager",
-                                                     G_APP_INFO_CREATE_SUPPORTS_STARTUP_NOTIFICATION, NULL);
-      if (!g_app_info_launch (app_info, NULL, NULL, &error))
-      {
-        if (error != NULL)
-        {
-          g_warning ("xfce4-power-manager could not be launched: %s", error->message);
-          g_error_free (error);
-          error = NULL;
-        }
-      }
-      /* wait 2 seconds for xfpm to startup */
-      g_usleep (2 * 1000000);
-    }
-    else
-    {
-      /* exit without starting xfpm */
-      return;
-    }
-  }
-
-  if (!xfconf_init (&error))
-  {
-    g_critical ("xfconf init failed: %s using default settings", error->message);
-    xfce_dialog_show_warning (NULL, _("Xfce Power Manager"),
-                              "%s", _("Failed to load power manager configuration, using defaults"));
-    g_clear_error (&error);
-  }
-
-
   channel = xfconf_channel_new (XFPM_CHANNEL);
-
   xfpm_debug_init (priv->debug);
 
   hash = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, g_free);
@@ -238,7 +201,6 @@ xfpm_settings_app_launch (GApplication *app)
   }
   g_variant_iter_free (iter);
   g_variant_unref (config);
-
 
   has_battery = xfpm_string_to_bool (g_hash_table_lookup (hash, "has-battery"));
   has_lid = xfpm_string_to_bool (g_hash_table_lookup (hash, "has-lid"));
@@ -268,11 +230,91 @@ xfpm_settings_app_launch (GApplication *app)
 #endif
                                      priv->device_id, GTK_APPLICATION (app));
 
-  g_hash_table_destroy (hash);
-
   gtk_application_add_window (GTK_APPLICATION (app), GTK_WINDOW (dialog));
+  g_hash_table_destroy (hash);
+}
 
-  g_object_unref (manager);
+static void
+xfpm_settings_app_launch (GApplication *app)
+{
+  XfpmSettingsAppPrivate *priv = xfpm_settings_app_get_instance_private (XFPM_SETTINGS_APP (app));
+  XfpmPowerManager *manager;
+  GError *error = NULL;
+  GList *windows = gtk_application_get_windows (GTK_APPLICATION (app));
+  gchar *owner;
+
+  if (windows != NULL)
+  {
+    XFPM_DEBUG ("window already opened");
+
+    gtk_window_present (windows->data);
+    gdk_notify_startup_complete ();
+
+    if (priv->device_id != NULL)
+    {
+      xfpm_settings_show_device_id (priv->device_id);
+    }
+
+    return;
+  }
+
+  if (!xfconf_init (&error))
+  {
+    g_critical ("xfconf init failed: %s", error->message);
+    xfce_dialog_show_warning (NULL, _("Xfce Power Manager"),
+                              "%s", _("Failed to load power manager configuration"));
+    g_error_free (error);
+    return;
+  }
+
+  manager = xfpm_power_manager_proxy_new_for_bus_sync (G_BUS_TYPE_SESSION,
+                                                       G_DBUS_PROXY_FLAGS_NONE,
+                                                       "org.xfce.PowerManager",
+                                                       "/org/xfce/PowerManager",
+                                                       NULL,
+                                                       &error);
+  if (error != NULL)
+  {
+    g_critical ("xfpm_power_manager_proxy_new_sync failed: %s", error->message);
+    xfce_dialog_show_warning (NULL, _("Xfce Power Manager"),
+                              "%s", _("Failed to connect to power manager"));
+    g_error_free (error);
+    return;
+  }
+
+  owner = g_dbus_proxy_get_name_owner (G_DBUS_PROXY (manager));
+  if (owner != NULL)
+  {
+    g_application_hold (G_APPLICATION (app));
+    get_config (manager, NULL, XFPM_SETTINGS_APP (app));
+    g_free (owner);
+  }
+  else
+  {
+    GtkWidget *dialog = gtk_message_dialog_new (
+      NULL, GTK_DIALOG_MODAL, GTK_MESSAGE_QUESTION, GTK_BUTTONS_YES_NO,
+      _("Xfce4 Power Manager is not running, do you want to launch it now?"));
+    g_signal_connect_after (dialog, "response", G_CALLBACK (gtk_widget_destroy), NULL);
+
+    if (gtk_dialog_run (GTK_DIALOG (dialog)) == GTK_RESPONSE_YES)
+    {
+      g_signal_connect (manager, "notify::g-name-owner", G_CALLBACK (get_config), app);
+      g_application_hold (G_APPLICATION (app));
+
+      if (!g_spawn_command_line_async ("xfce4-power-manager", &error))
+      {
+        g_critical ("Failed to start xfce4-power-manager: %s", error->message);
+        xfce_dialog_show_warning (NULL, _("Xfce Power Manager"),
+                                  "%s", _("Failed to connect to power manager"));
+        g_application_release (G_APPLICATION (app));
+        g_error_free (error);
+      }
+      else
+      {
+        priv->abort_id = g_timeout_add_seconds (5, abort_xfpm_startup, app);
+      }
+    }
+  }
 }
 
 static void
@@ -412,6 +454,7 @@ xfpm_settings_app_class_init (XfpmSettingsAppClass *class)
   gapplication_class->handle_local_options = xfpm_settings_app_local_options;
   gapplication_class->startup = xfpm_settings_app_startup;
   gapplication_class->activate = xfpm_settings_app_activate;
+  gapplication_class->shutdown = xfpm_settings_app_shutdown;
 }
 
 XfpmSettingsApp *
