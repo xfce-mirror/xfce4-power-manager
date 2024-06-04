xfce-mirror/xfce4-power-manager

commit d1f874ac1eccf11b6b061e96fbc6ceebffece504

Properly check for multi-monitor setup
As far as possible anyway, especially on Wayland where the
wlr-output-management protocol may not be available.

Fixes: #82
Helps: #1
Related: #2

common/xfpm-common.c
@@ -25,6 +25,16 @@
 #include "xfpm-common.h"
 #include "xfpm-debug.h"
 
+#ifdef ENABLE_X11
+#include <X11/extensions/Xrandr.h>
+#include <gdk/gdkx.h>
+#endif
+
+#ifdef ENABLE_WAYLAND
+#include "protocols/wlr-output-management-unstable-v1-client.h"
+#include <gdk/gdkwayland.h>
+#endif
+
 #include <libxfce4util/libxfce4util.h>
 
 const gchar *
@@ -125,29 +135,6 @@ xfpm_about (gpointer data)
                          NULL);
 }
 
-gboolean
-xfpm_is_multihead_connected (void)
-{
-  GdkDisplay *dpy;
-  gint nmonitor;
-
-  dpy = gdk_display_get_default ();
-
-  nmonitor = gdk_display_get_n_monitors (dpy);
-
-  if (nmonitor > 1)
-  {
-    XFPM_DEBUG ("Multiple monitors connected");
-    return TRUE;
-  }
-  else
-  {
-    return FALSE;
-  }
-
-  return FALSE;
-}
-
 GdkPixbuf *
 xfpm_icon_load (const gchar *icon_name,
                 gint size,
@@ -170,4 +157,194 @@ xfpm_icon_load (const gchar *icon_name,
   }
 
   return pix;
+}
+
+#ifdef ENABLE_WAYLAND
+/* clang-format off */
+static void registry_global (void *data, struct wl_registry *registry, uint32_t id, const char *interface, uint32_t version);
+static void registry_global_remove (void *data, struct wl_registry *registry, uint32_t id) {}
+static void manager_head (void *data, struct zwlr_output_manager_v1 *wl_manager, struct zwlr_output_head_v1 *head);
+static void manager_done (void *data, struct zwlr_output_manager_v1 *wl_manager, uint32_t serial) {}
+static void manager_finished (void *data, struct zwlr_output_manager_v1 *wl_manager);
+static void head_name (void *data, struct zwlr_output_head_v1 *head, const char *name) {}
+static void head_description (void *data, struct zwlr_output_head_v1 *head, const char *description) {}
+static void head_physical_size (void *data, struct zwlr_output_head_v1 *head, int32_t width, int32_t height) {}
+static void head_mode (void *data, struct zwlr_output_head_v1 *head, struct zwlr_output_mode_v1 *wl_mode) {}
+static void head_enabled (void *data, struct zwlr_output_head_v1 *head, int32_t enabled) {}
+static void head_current_mode (void *data, struct zwlr_output_head_v1 *head, struct zwlr_output_mode_v1 *wl_mode) {}
+static void head_position (void *data, struct zwlr_output_head_v1 *head, int32_t x, int32_t y) {}
+static void head_transform (void *data, struct zwlr_output_head_v1 *head, int32_t transform) {}
+static void head_scale (void *data, struct zwlr_output_head_v1 *head, wl_fixed_t scale) {}
+static void head_finished (void *data, struct zwlr_output_head_v1 *head);
+static void head_make (void *data, struct zwlr_output_head_v1 *head, const char *make) {}
+static void head_model (void *data, struct zwlr_output_head_v1 *head, const char *model) {}
+static void head_serial_number (void *data, struct zwlr_output_head_v1 *head, const char *serial_number) {}
+static void head_adaptive_sync (void *data, struct zwlr_output_head_v1 *head, uint32_t state) {}
+/* clang-format on */
+
+static const struct wl_registry_listener registry_listener = {
+  .global = registry_global,
+  .global_remove = registry_global_remove,
+};
+
+static const struct zwlr_output_manager_v1_listener manager_listener = {
+  .head = manager_head,
+  .done = manager_done,
+  .finished = manager_finished,
+};
+
+static const struct zwlr_output_head_v1_listener head_listener = {
+  .name = head_name,
+  .description = head_description,
+  .physical_size = head_physical_size,
+  .mode = head_mode,
+  .enabled = head_enabled,
+  .current_mode = head_current_mode,
+  .position = head_position,
+  .transform = head_transform,
+  .scale = head_scale,
+  .finished = head_finished,
+  .make = head_make,
+  .model = head_model,
+  .serial_number = head_serial_number,
+  .adaptive_sync = head_adaptive_sync,
+};
+
+typedef struct _XfpmMultiheadData
+{
+  struct wl_registry *wl_registry;
+  struct zwlr_output_manager_v1 *wl_manager;
+  GList *heads;
+} XfpmMultiheadData;
+
+static void
+xfpm_multihead_data_free (gpointer _data)
+{
+  XfpmMultiheadData *data = _data;
+  if (data->wl_manager != NULL)
+    zwlr_output_manager_v1_destroy (data->wl_manager);
+  g_list_free_full (data->heads, (GDestroyNotify) zwlr_output_head_v1_destroy);
+  wl_registry_destroy (data->wl_registry);
+  g_free (data);
+}
+
+static void
+registry_global (void *_data,
+                 struct wl_registry *registry,
+                 uint32_t id,
+                 const char *interface,
+                 uint32_t version)
+{
+  XfpmMultiheadData *data = _data;
+  if (g_strcmp0 (zwlr_output_manager_v1_interface.name, interface) == 0)
+    data->wl_manager = wl_registry_bind (data->wl_registry, id, &zwlr_output_manager_v1_interface,
+                                         MIN ((uint32_t) zwlr_output_manager_v1_interface.version, version));
+}
+
+static void
+manager_head (void *_data,
+              struct zwlr_output_manager_v1 *wl_manager,
+              struct zwlr_output_head_v1 *head)
+{
+  XfpmMultiheadData *data = _data;
+  data->heads = g_list_prepend (data->heads, head);
+  zwlr_output_head_v1_add_listener (head, &head_listener, data);
+}
+
+static void
+manager_finished (void *_data,
+                  struct zwlr_output_manager_v1 *_wl_manager)
+{
+  XfpmMultiheadData *data = _data;
+  zwlr_output_manager_v1_destroy (data->wl_manager);
+  data->wl_manager = NULL;
+}
+
+static void
+head_finished (void *_data,
+               struct zwlr_output_head_v1 *head)
+{
+  XfpmMultiheadData *data = _data;
+  data->heads = g_list_remove (data->heads, head);
+  zwlr_output_head_v1_destroy (head);
+}
+#endif
+
+gboolean
+xfpm_is_multihead_connected (GObject *_lifetime)
+{
+  static GObject *lifetime = NULL;
+  static gboolean native_checked = FALSE;
+  static gboolean native_available = TRUE;
+
+  GdkDisplay *display = gdk_display_get_default ();
+
+  g_return_val_if_fail (lifetime == NULL || lifetime == _lifetime, FALSE);
+
+#ifdef ENABLE_X11
+  if (native_available && GDK_IS_X11_DISPLAY (display))
+  {
+    XRRScreenResources *resources;
+    gboolean multihead_connected;
+    Display *xdisplay = gdk_x11_get_default_xdisplay ();
+
+    if (!native_checked)
+    {
+      int event_base, error_base;
+      native_available = XRRQueryExtension (xdisplay, &event_base, &error_base);
+      native_checked = TRUE;
+
+      if (!native_available)
+      {
+        g_warning ("No Xrandr extension found, falling back to GDK output detection");
+        return gdk_display_get_n_monitors (display) > 1;
+      }
+    }
+
+    resources = XRRGetScreenResourcesCurrent (xdisplay, gdk_x11_get_default_root_xwindow ());
+    multihead_connected = resources->noutput > 1;
+    XRRFreeScreenResources (resources);
+
+    return multihead_connected;
+  }
+#endif
+
+#ifdef ENABLE_WAYLAND
+  if (native_available && GDK_IS_WAYLAND_DISPLAY (display))
+  {
+    static XfpmMultiheadData *data = NULL;
+
+    if (!native_checked)
+    {
+      struct wl_display *wl_display = gdk_wayland_display_get_wl_display (display);
+
+      data = g_new0 (XfpmMultiheadData, 1);
+      data->wl_registry = wl_display_get_registry (wl_display);
+      wl_registry_add_listener (data->wl_registry, &registry_listener, data);
+      wl_display_roundtrip (wl_display);
+      native_available = data->wl_manager != NULL;
+      native_checked = TRUE;
+
+      if (native_available)
+      {
+        lifetime = _lifetime;
+        g_object_weak_ref (lifetime, (GWeakNotify) (void (*) (void)) xfpm_multihead_data_free, data);
+        zwlr_output_manager_v1_add_listener (data->wl_manager, &manager_listener, data);
+        wl_display_roundtrip (wl_display);
+      }
+      else
+      {
+        xfpm_multihead_data_free (data);
+        g_warning ("Your compositor does not seem to support the wlr-output-management protocol:"
+                   "falling back to GDK output detection");
+        return gdk_display_get_n_monitors (display) > 1;
+      }
+    }
+
+    return g_list_length (data->heads) > 1;
+  }
+#endif
+
+  /* fallback: wrong if laptop screen is disabled and external screen is enabled */
+  return gdk_display_get_n_monitors (display) > 1;
 }

common/Makefile.am
@@ -46,6 +46,15 @@ libxfpmcommon_la_LIBADD += \
 	$(XRANDR_LIBS)
 endif
 
+if ENABLE_WAYLAND
+libxfpmcommon_la_CFLAGS += \
+	$(WAYLAND_CLIENT_CFLAGS)
+
+libxfpmcommon_la_LIBADD += \
+	$(top_builddir)/protocols/libprotocols.la \
+	$(WAYLAND_CLIENT_LIBS)
+endif
+
 if HAVE_POLKIT
 libxfpmcommon_la_SOURCES += \
 	xfpm-brightness-polkit.c \

common/xfpm-common.h
@@ -45,7 +45,7 @@ xfpm_quit (void);
 void
 xfpm_about (gpointer data);
 gboolean
-xfpm_is_multihead_connected (void);
+xfpm_is_multihead_connected (GObject *lifetime);
 
 G_END_DECLS
 

protocols/Makefile.am
@@ -12,7 +12,10 @@ libprotocols_built_sources = \
 	ext-idle-notify-v1.c \
 	ext-idle-notify-v1-client.h \
 	wlr-output-power-management-unstable-v1.c \
-	wlr-output-power-management-unstable-v1-client.h
+	wlr-output-power-management-unstable-v1-client.h \
+	wlr-output-management-unstable-v1.c \
+	wlr-output-management-unstable-v1-client.h \
+	$(NULL)
 
 nodist_libprotocols_la_SOURCES = \
 	$(libprotocols_built_sources)
@@ -71,4 +74,6 @@ EXTRA_DIST += \
 endif
 
 EXTRA_DIST += \
-	wlr-protocols/unstable/wlr-output-power-management-unstable-v1.xml
+	wlr-protocols/unstable/wlr-output-power-management-unstable-v1.xml \
+	wlr-protocols/unstable/wlr-output-management-unstable-v1.xml \
+	$(NULL)

src/xfpm-manager.c
@@ -459,12 +459,12 @@ xfpm_manager_lid_changed_cb (XfpmPower *power,
 
     if (action == LID_TRIGGER_DPMS)
     {
-      if (manager->priv->dpms != NULL && !xfpm_is_multihead_connected ())
+      if (manager->priv->dpms != NULL && !xfpm_is_multihead_connected (G_OBJECT (manager)))
         xfpm_dpms_set_mode (manager->priv->dpms, XFPM_DPMS_MODE_OFF);
     }
     else if (action == LID_TRIGGER_LOCK_SCREEN)
     {
-      if (!xfpm_is_multihead_connected ())
+      if (!xfpm_is_multihead_connected (G_OBJECT (manager)))
       {
         if (!xfce_screensaver_lock (manager->priv->screensaver))
         {

commit 01571e513d7be36be93b1d99be9059786788275e

x11: common: Fix multi-monitor setup detection
Closes: #232
Fixes: d1f874ac1eccf11b6b061e96fbc6ceebffece504

common/xfpm-common.c
@@ -286,7 +286,7 @@ xfpm_is_multihead_connected (GObject *_lifetime)
   if (native_available && GDK_IS_X11_DISPLAY (display))
   {
     XRRScreenResources *resources;
-    gboolean multihead_connected;
+    gboolean n_connected_outputs = 0;
     Display *xdisplay = gdk_x11_get_default_xdisplay ();
 
     if (!native_checked)
@@ -303,10 +303,16 @@ xfpm_is_multihead_connected (GObject *_lifetime)
     }
 
     resources = XRRGetScreenResourcesCurrent (xdisplay, gdk_x11_get_default_root_xwindow ());
-    multihead_connected = resources->noutput > 1;
+    for (gint n = 0; n < resources->noutput; n++)
+    {
+      XRROutputInfo *output_info = XRRGetOutputInfo (xdisplay, resources, resources->outputs[n]);
+      if (output_info->connection == RR_Connected)
+        n_connected_outputs++;
+      XRRFreeOutputInfo (output_info);
+    }
     XRRFreeScreenResources (resources);
 
-    return multihead_connected;
+    return n_connected_outputs > 1;
   }
 #endif
 