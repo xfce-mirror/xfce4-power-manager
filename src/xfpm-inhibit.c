b5dd7130fe79fbe586b40c16d65ba2378dadbc3e
Manu Barrio Linares <mbarriolinares@gmail.com>
2024-11-17 21:11:09 +0000

    Update file xfpm-inhibit.c

--- src/xfpm-inhibit.c
+++ src/xfpm-inhibit.c
@@ -182,10 +182,10 @@ xfpm_inhibit_connection_lost_cb (XfpmDBusMonitor *monitor,
     return;
 
   guint i;
-  for ( i = inhibit->priv->array->len - 1; i != (guint) -1; i--)
+  for (i = inhibit->priv->array->len - 1; i != (guint) -1; i--)
   {
     inhibitor = g_ptr_array_index (inhibit->priv->array, i);
-    if ( g_strcmp0 (inhibitor->unique_name, unique_name ) == 0 )
+    if (g_strcmp0 (inhibitor->unique_name, unique_name ) == 0 )
     {
       XFPM_DEBUG ("Application=%s with unique connection name=%s disconnected",
                   inhibitor->app_name, inhibitor->unique_name);
