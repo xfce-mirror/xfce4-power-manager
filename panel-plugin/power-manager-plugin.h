xfce-mirror/xfce4-power-manager

commit 4c8aff2b1017c8a0c0e1642613647c8dca82bd9f

Cleanup and formatting suggested by gobject-linter

panel-plugin/power-manager-plugin.h
@@ -27,9 +27,6 @@ G_BEGIN_DECLS
 #define POWER_MANAGER_TYPE_PLUGIN (power_manager_plugin_get_type ())
 G_DECLARE_FINAL_TYPE (PowerManagerPlugin, power_manager_plugin, POWER_MANAGER, PLUGIN, XfcePanelPlugin)
 
-void
-power_manager_plugin_register_type (XfcePanelTypeModule *type_module);
-
 G_END_DECLS
 
 #endif /* !__POWER_MANAGER_PLUGIN_H__ */

libdbus/xfpm-dbus-monitor.h
@@ -67,8 +67,6 @@ GType
 xfpm_dbus_monitor_get_type (void) G_GNUC_CONST;
 XfpmDBusMonitor *
 xfpm_dbus_monitor_new (void);
-void
-xfpm_dbus_monitor_watch_system_bus (XfpmDBusMonitor *monitor);
 gboolean
 xfpm_dbus_monitor_add_unique_name (XfpmDBusMonitor *monitor,
                                    GBusType bus_type,

panel-plugin/power-manager-config.c
@@ -70,7 +70,7 @@ struct _PowerManagerConfig
 };
 
 
-G_DEFINE_TYPE (PowerManagerConfig, power_manager_config, G_TYPE_OBJECT)
+G_DEFINE_FINAL_TYPE (PowerManagerConfig, power_manager_config, G_TYPE_OBJECT)
 
 
 

panel-plugin/power-manager-dialog.c
@@ -66,7 +66,7 @@ struct _PowerManagerDialog
 };
 
 
-G_DEFINE_TYPE (PowerManagerDialog, power_manager_dialog, G_TYPE_OBJECT)
+G_DEFINE_FINAL_TYPE (PowerManagerDialog, power_manager_dialog, G_TYPE_OBJECT)
 
 
 

panel-plugin/scalemenuitem.c
@@ -78,7 +78,7 @@ enum
 static guint signals[LAST_SIGNAL] = { 0 };
 
 G_GNUC_BEGIN_IGNORE_DEPRECATIONS
-G_DEFINE_TYPE (XfpmScaleMenuItem, xfpm_scale_menu_item, GTK_TYPE_IMAGE_MENU_ITEM)
+G_DEFINE_FINAL_TYPE (XfpmScaleMenuItem, xfpm_scale_menu_item, GTK_TYPE_IMAGE_MENU_ITEM)
 G_GNUC_END_IGNORE_DEPRECATIONS
 
 

settings/xfpm-settings-app.h
@@ -29,15 +29,16 @@
 G_BEGIN_DECLS
 
 
+typedef struct _XfpmSettingsApp XfpmSettingsApp;
+typedef struct _XfpmSettingsAppClass XfpmSettingsAppClass;
+typedef struct _XfpmSettingsAppPrivate XfpmSettingsAppPrivate;
+
 #define XFPM_TYPE_SETTINGS_APP (xfpm_settings_app_get_type ())
 #define XFPM_SETTINGS_APP(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), XFPM_TYPE_SETTINGS_APP, XfpmSettingsApp))
 #define XFPM_SETTINGS_APP_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), XFPM_TYPE_SETTINGS_APP, XfpmSettingsAppClass))
 #define XFPM_IS_SETTINGS_APP(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), XFPM_TYPE_SETTINGS_APP))
 #define XFPM_SETTINGS_APP_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), XFPM_TYPE_SETTINGS_APP, XfpmSettingsAppClass))
-
-typedef struct _XfpmSettingsApp XfpmSettingsApp;
-typedef struct _XfpmSettingsAppClass XfpmSettingsAppClass;
-typedef struct _XfpmSettingsAppPrivate XfpmSettingsAppPrivate;
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (XfpmSettingsApp, g_object_unref)
 
 struct _XfpmSettingsApp
 {

src/xfpm-backlight-helper.c
@@ -209,7 +209,7 @@ backlight_helper_get_device (void)
   gchar *device = NULL;
   gint i;
 
-  device = (gchar *) g_malloc (sizeof (gchar));
+  device = g_new (gchar, 1);
 
   for (i = 0; types[i] != NULL; i++)
   {

src/xfpm-inhibit.h
@@ -50,8 +50,6 @@ typedef struct
 
 GType
 xfpm_inhibit_get_type (void) G_GNUC_CONST;
-GType
-xfpm_inhibit_error_get_type (void) G_GNUC_CONST;
 GQuark
 xfpm_inhibit_get_error_quark ();
 XfpmInhibit *

commit 25b620fc3c75db76093d1a12c3e3495412c41ab5

Fix previous commit
Fixes: 4c8aff2b1017c8a0c0e1642613647c8dca82bd9f

panel-plugin/power-manager-plugin.h
@@ -27,6 +27,9 @@ G_BEGIN_DECLS
 #define POWER_MANAGER_TYPE_PLUGIN (power_manager_plugin_get_type ())
 G_DECLARE_FINAL_TYPE (PowerManagerPlugin, power_manager_plugin, POWER_MANAGER, PLUGIN, XfcePanelPlugin)
 
+void
+power_manager_plugin_register_type (XfcePanelTypeModule *type_module);
+
 G_END_DECLS
 
 #endif /* !__POWER_MANAGER_PLUGIN_H__ */